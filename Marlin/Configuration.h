92ad618de5f3c85ce5f71efcaf11359a168a8b57
David Whittington <djwhitt@gmail.com>
2013-03-10 17:11:51 -0400

    Fine tuning E steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -279,7 +279,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,2015.748,760 * 1.1}  // default steps per unit for ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,2015.748,760 * 1.128}  // default steps per unit for ultimaker
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 45}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
